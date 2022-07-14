c7a794f33b67294b3e5af77bc20bc90d881ce161
hiro <hiromhiro617@gmail.com>
2022-07-14 22:47:27 +0900

    init

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'sinatra-websocket'
+require "sinatra/json"
+
+set :server, 'thin'
+set :sockets, []
+
+get '/' do
+  json status: 'ok'
+end
+
+get '/websocket' do
+  if request.websocket?
+    request.websocket do |ws|
+      ws.onopen do
+        settings.sockets << ws
+      end
+      ws.onmessage do |msg|
+        settings.sockets.each do |s|
+          s.send(msg)
+        end
+      end
+      ws.onclose do
+        settings.sockets.delete(ws)
+      end
+    end
+  end
+end
